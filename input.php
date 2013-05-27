bb5ec427b8e77bb80fc3e197c7164e8dfb9a7f3e
Patrick Sean Collins <pscollins@as.cs.uchicago.edu>
2013-05-27 03:39:25 -0500

    update

---
+++ input.php
@@ -0,0 +1,3 @@
+<?
+/* It's probably best to wrap the database creation into a function in a header file  */
+$db = new mysqli(HOST, USER, PASSWORD, DATABASE)
--- register_printer.php
+++ register_printer.php
@@ -1,23 +1,34 @@
 <?
+/* Takes an array of 3-tuples for $form_items of the format: */
+/*    array("descrpitive text", "input type", "input name") */
+function formTable($form_vals, $page, $method)
+{
+  echo "<table border = \"1\"> ";
+  echo "<form method = \"$method\" action = \"$page\"> ";
+  foreach ($form_vals as $form_val){
+    formRow($form_val);
+  }
+  echo "</form> ";
+  echo "</table ";
+}
 
-
-function formBuilder($form_items, $page, $method)
-
-function formCell
-
-function formEntry($form_vals){
-echo "<input type = \"
-
+/* Helper function for formBuilder  */
+function formRow($form_val) 
+{
+  echo "<tr> "
+  echo "<td> $form_val[0] </td> ";
+  echo "<td> " 
+  echo "<input type = \"$form_val[1]\" name = \"form_val[2]\"> ";
+  echo "</td> ";
+  echo "</tr>";
+}
 
 $form_items = array( array("Room number", "text", "room_number"),
 		     array("Printer name", "text", "host_name"),
 		     array("Host name", "text", "host_name"),
 		     array("Description", "text", "description"),
 		     array("", "hidden", "timestamp") )
+?>
 
-<form action="register.php" method = "get">
-
-
-
-Room number: <input type ="text" name ="room_number">
+<? formTable($form_items, "input.php", "get"); ?>
 
